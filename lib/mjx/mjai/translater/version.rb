3c793979d030af14ccedc40a75567a9c92ab35d8
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-03-08 11:59:47 +0900

    Revert "bundle gem mjx-mjai-translater (#14)" (#15)
    This reverts commit 8349b34b12acc5d71cdf5a8b4f71fc90fbbe267c.

--- lib/mjx/mjai/translater/version.rb
+++
@@ -1,7 +0,0 @@
-module Mjx
-  module Mjai
-    module Translater
-      VERSION = "0.1.0"
-    end
-  end
-end
--- spec/mjx/mjai/translater_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Mjx::Mjai::Translater do
-  it "has a version number" do
-    expect(Mjx::Mjai::Translater::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
