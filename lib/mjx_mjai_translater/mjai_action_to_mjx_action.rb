d7e308c6f003e4bb3c1adbf178d29a1a03da59b4
nissymori <kyoudai.630@gmail.com>
2021-03-08 15:19:24 +0900

    create frame

---
+++ lib/mjx_mjai_translater/mjai_action_to_mjx_action.rb
@@ -0,0 +1,15 @@
+require "./open_converter.rb"
+
+
+
+def mjai_possible_actions(proto_possible_actions)
+  #mjxのpossibleactionsをmjaiのフォーマットに符合する。
+  return [{}, {}]
+
+def find_proper_action_idx(mjai_action, mjai_possible_actions)
+  return  0
+
+def mjai_act_to_mjx_act(mjai_action, proto_possible_actions)
+  mjai_possible_actions = mjai_possible_actions(proto_possible_actions)  #possible actions をmjaiのフォーマットに変換する
+  proper_action_idx = find_proper_action_idx(mjai_action, mjai_possible_actions)
+  return proto_possible_actions[proper_action_idx]
---
+++ lib/mjx_mjai_translater/open_converter.rb
@@ -0,0 +1,8 @@
+class OpenConverter
+  def initialize(open)
+    @open = open
+  end
+#必要な情報
+# -晒した牌の情報 
+
+end
