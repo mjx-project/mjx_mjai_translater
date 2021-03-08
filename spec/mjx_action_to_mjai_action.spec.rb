541ccd45f529b45c1225e617170a35248ff044ad
nissymori <kyoudai.630@gmail.com>
2021-03-08 16:36:42 +0900

    rename

--- spec/mjx_action_to_mjai_action.spec.rb
+++ spec/mjx_action_to_mjai_action.spec.rb
@@ -3,6 +3,6 @@ require "../lib/mjx_mjai_translater/mjx_action_to_mjai_action"
 
 Rspec.describe  MjxActToMjaiAct do
   it "protoのtileがmjaiのtileに変換できること" do
-    expect(Calc.new({}).proto_tile_to_mjai_tile(16)).to eq "5mr"
+    expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(16)).to eq "5mr"
   end
 end
