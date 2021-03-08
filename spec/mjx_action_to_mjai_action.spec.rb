70abbec061efc0c298a35eedf52b180e65d45ada
nissymori <kyoudai.630@gmail.com>
2021-03-08 16:46:26 +0900

    refactor test and MjxActToMjaiAct

--- spec/mjx_action_to_mjai_action.spec.rb
+++ spec/mjx_action_to_mjai_action.spec.rb
@@ -1,7 +1,7 @@
-require "../lib/mjx_mjai_translater/mjx_action_to_mjai_action"
+require "./lib/mjx_mjai_translater/mjx_action_to_mjai_action"
 
 
-Rspec.describe  MjxActToMjaiAct do
+RSpec.describe  MjxActToMjaiAct do
   it "protoのtileがmjaiのtileに変換できること" do
     expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(16)).to eq "5mr"
   end
