9508403057ec846a24cd866a3b131f2923611938
nissymori <kyoudai.630@gmail.com>
2021-03-08 16:35:23 +0900

    add red_test_case

--- spec/mjx_action_to_mjai_action.spec.rb
+++ spec/mjx_action_to_mjai_action.spec.rb
@@ -1,1 +1,8 @@
 require "../lib/mjx_mjai_translater/mjx_action_to_mjai_action"
+
+
+Rspec.describe  MjxActToMjaiAct do
+  it "protoのtileがmjaiのtileに変換できること" do
+    expect(Calc.new({}).proto_tile_to_mjai_tile(16)).to eq "5mr"
+  end
+end
