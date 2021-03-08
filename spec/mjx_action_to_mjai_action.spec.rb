d3292a24c8bc1b3d01718723a953fdff86644477
nissymori <kyoudai.630@gmail.com>
2021-03-08 17:09:28 +0900

    convert other than zihai

--- spec/mjx_action_to_mjai_action.spec.rb
+++ spec/mjx_action_to_mjai_action.spec.rb
@@ -2,7 +2,10 @@ require "./lib/mjx_mjai_translater/mjx_action_to_mjai_action"
 
 
 RSpec.describe  MjxActToMjaiAct do
-  it "protoのtileがmjaiのtileに変換できること" do
+  it "protoの赤tileがmjaiのtileに変換できること" do
     expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(16)).to eq "5mr"
+  end
+  it "protoの赤以外の数牌をmjaiのtileに変換できること" do
+    expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(0)).to eq "1m"
   end
 end
