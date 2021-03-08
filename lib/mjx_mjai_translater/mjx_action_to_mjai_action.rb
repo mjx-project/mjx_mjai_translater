0ca31b74c9005e47bf78fc8e81ca50233d4731bb
nissymori <kyoudai.630@gmail.com>
2021-03-08 20:27:07 +0900

    implement tile_converter

--- lib/mjx_mjai_translater/mjx_action_to_mjai_action.rb
+++ lib/mjx_mjai_translater/mjx_action_to_mjai_action.rb
@@ -12,12 +12,14 @@ class MjxActToMjaiAct
   def proto_tile_to_mjai_tile(proto_tile)  # 他のクラスに
     reds_in_proto = [16, 52, 88]
     reds_dict = {16 => "5mr", 52 => "5sr", 88 => "5pr"}
-    m_kind_dict = {0 => "m", 1 => "s", 2 => "p"}
-    if reds_in_proto.include?(proto_tile)
+    mod36_kind_dict = {0 => "m", 1 => "s", 2 => "p"}
+    num_zihai_dict = {0 => "E", 1 => "S", 2 => "W", 3 => "N", 4 => "P", 5 => "F", 6 => "C"}
+    if reds_in_proto.include?(proto_tile)  # 赤
       return reds_dict[proto_tile]
     end
-    if proto_tile.div(36) <= 2
-      return (proto_tile.div(36).quo(4) + 1).to_i.to_s + m_kind_dict[proto_tile.div(36)]
+    if proto_tile.div(36) < 2  #数牌
+      return ((proto_tile % 36 ).div(4) + 1).to_i.to_s + mod36_kind_dict[proto_tile.div(36)]
     end
+    return num_zihai_dict[(proto_tile % 36).div(4)]  #字牌
   end
 end
--- spec/mjx_action_to_mjai_action.spec.rb
+++ spec/mjx_action_to_mjai_action.spec.rb
@@ -6,6 +6,9 @@ RSpec.describe  MjxActToMjaiAct do
     expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(16)).to eq "5mr"
   end
   it "protoの赤以外の数牌をmjaiのtileに変換できること" do
-    expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(0)).to eq "1m"
+    expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(17)).to eq "5m"
+  end
+  it "protoの字牌をmjaiのtileに変換できるか" do
+    expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(131)).to eq "F"
   end
 end
