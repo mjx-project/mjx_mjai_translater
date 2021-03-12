60a0ccd080c8141fcc45e6e5fe9cbb95d6926b33
Nishimori Soichiro <55940613+nissymori@users.noreply.github.com>
2021-03-12 10:23:28 +0900

    赤以外のtileを変換する。 (#26)
    * init
    
    * convert other than zihai
    
    * implement tile_converter

--- spec/mjx_action_to_mjai_action.spec.rb
+++ spec/mjx_action_to_mjai_action.spec.rb
@@ -2,7 +2,13 @@ require "./lib/mjx_mjai_translater/mjx_action_to_mjai_action"
 
 
 RSpec.describe  MjxActToMjaiAct do
-  it "protoのtileがmjaiのtileに変換できること" do
+  it "protoの赤tileがmjaiのtileに変換できること" do
     expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(16)).to eq "5mr"
+  end
+  it "protoの赤以外の数牌をmjaiのtileに変換できること" do
+    expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(17)).to eq "5m"
+  end
+  it "protoの字牌をmjaiのtileに変換できるか" do
+    expect(MjxActToMjaiAct.new({}).proto_tile_to_mjai_tile(131)).to eq "F"
   end
 end
