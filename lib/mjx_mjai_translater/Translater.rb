364f7f4ceebf1561c900a816862c54dd5466c298
nissymori <kyoudai.630@gmail.com>
2021-03-12 11:23:40 +0900

    add frame of translater

--- lib/mjx_mjai_translater/Translater.rb
+++ lib/mjx_mjai_translater/Translater.rb
@@ -1,2 +1,48 @@
+require "random_agent"
 #変換サーバの本体
 
+class TransServer << RandomAgent
+    
+    def initialize()
+        self.players = []
+        self._mjx_event_history = []
+        self.new_mjai_acitons = []
+        self.next_mjx_actions = []
+    end
+
+
+    def do_action(action)
+        #mjaiと同じ実装
+    end
+
+
+    def step(new_event):
+        # do_actionの呼ばれ方がActiveGame内で11パターンあったので、それらを模倣する
+    end
+    
+
+    def update_next_actions(response)
+        #　ユーザーのアクションに対してmjaiのアクションからmjxのアクションに変更する
+    end
+
+
+    def get_curr_player(observation)
+    end
+
+    
+    def observe(observation)
+    end
+
+    def take_action(obser_vation)
+        obserbve(observation)
+        curr_player = get_curr_player(observation)
+        response = none
+        for mjai_action in self.mjai_new_actinos:
+            response = self.do_action(mjai_action)
+        self.next_mjx_actions = update_next_actions(response)
+        return self.next_mjx_actions[curr_player]
+    end
+
+
+
+end
