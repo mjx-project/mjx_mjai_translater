3638cd675ade78f295ee542d0eb55043a36792fd
nissymori <kyoudai.630@gmail.com>
2021-03-26 15:52:42 +0900

    add test about intermediate

--- spec/extract_observation_difference_spec.rb
+++ spec/extract_observation_difference_spec.rb
@@ -19,7 +19,13 @@ RSpec.describe  TransServer do
     it "局の最初" do
         observation = observation_from_json(0)
         difference_extracted = TransServer.new().extract_difference(None, observation)  # 差分を取得する関数を動かす                          
-        expect(difference_extracted).to eq  {"init_hand"[43,45,93,113,49,101,80,40,70,95,19,109,4], "draw":[28]}
+        expect(difference_extracted).to eq  [{"init_hand"[43,45,93,113,49,101,80,40,70,95,19,109,4]}, {"draw":28}]
+    end
+    it "局の途中"
+        observation_previous = observation_from_json(0)
+        observation = observation_from_json(1)
+        difference_extracted = TransServer.new().extract_difference(observation_previous, observation)
+        expect(difference_extracted).to eq [{"draw":55}, {"discard":109}]
     end
 end
 
