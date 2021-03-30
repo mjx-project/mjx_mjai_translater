3f6bdead15f90c2970f32903bf48581b8d16e81e
nissymori <kyoudai.630@gmail.com>
2021-03-30 23:08:39 +0900

    implement open event type

--- lib/mjx_mjai_translater/mjx_to_mjai.rb
+++ lib/mjx_mjai_translater/mjx_to_mjai.rb
@@ -1,6 +1,6 @@
 # possibleactionsをmjaiのactionのフォーマットに変換する
 $LOAD_PATH.unshift(__dir__) unless $LOAD_PATH.include?(__dir__)
-require "./open_converter.rb"
+require "open_converter.rb"
 this_dir = __dir__
 lib_dir = File.join(this_dir, '../mjxproto')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
--- lib/mjx_mjai_translater/open_converter.rb
+++ lib/mjx_mjai_translater/open_converter.rb
@@ -1,6 +1,6 @@
 class OpenConverter
   def initialize(open)
-    @open = open
+    @bits = open
   end
 #必要な情報
 # ポン,チー,ダイミンカン 
@@ -13,7 +13,21 @@ class OpenConverter
 # アンカン
 # consumed: 晒した牌
 
-  def open_event_type(open)
+
+  def open_event_type()  # rubyでは0はfalseを意味しない
+    if (1 << 2 & @bits) != 0
+      return "chi"
+    elsif 1 << 3 & @bits != 0
+      return "pon"
+    elsif 1 << 4 & @bits != 0
+      return "kakan"
+    else
+      if Mjxproto::RelativePos::RELATIVE_POS_SELF == @bits & 3
+          return "ankan"
+      else
+          return "daiminkan"
+      end
+    end
   end
 
 
@@ -26,6 +40,6 @@ class OpenConverter
   
   
   def open_tiles(open)
-  def
+  end
 
 end
