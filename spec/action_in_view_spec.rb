14dbb1f8575b0cab9113f5a428bdac781da9f6d7
nissymori <kyoudai.630@gmail.com>
2021-04-28 12:36:26 +0900

    add frame of test

---
+++ spec/action_in_view_spec.rb
@@ -0,0 +1,19 @@
+require './lib/mjx_mjai_translater/mjx_to_mjai'
+
+# action_in_view()はプレイヤーによって与える情報を調整するための関数
+
+RSpec.describe Action_in_view do
+    it "start_game" do
+    end
+    it "start_kyoku" do
+    end
+    it "tsumo" do
+    end
+    it "dahai kakan" do
+    end
+    it "chi pon" do
+    end
+    it "reach" do
+    end
+end
+
