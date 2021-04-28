91bd6bb55acf2c4b0ab80ca63e44cdc0c71f3cee
nissymori <kyoudai.630@gmail.com>
2021-04-28 12:43:24 +0900

    add frame

--- spec/action_in_view_spec.rb
+++ spec/action_in_view_spec.rb
@@ -2,7 +2,7 @@ require './lib/mjx_mjai_translater/mjx_to_mjai'
 
 # action_in_view()はプレイヤーによって与える情報を調整するための関数
 
-RSpec.describe Action_in_view do
+RSpec.describe "action_in_view" do
     it "start_game" do
     end
     it "start_kyoku" do
