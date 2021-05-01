060a1b4f6dcdbcbdd08628693e3701b71df49389
nissymori <kyoudai.630@gmail.com>
2021-05-01 15:10:48 +0900

    test

--- spec/action_in_view_spec.rb
+++ spec/action_in_view_spec.rb
@@ -2,6 +2,12 @@ require './lib/mjx_mjai_translater/trans_sever'
 require './lib/mjx_mjai_translater/trans_sever'
 
 # action_in_view()はプレイヤーによって与える情報を調整するための関数
+def observation_from_json(lines,line)  # コピペが多いのでこの関数用のファイルを別のPRデつくります。 
+    json = JSON.load(lines[line])
+    json_string = Google::Protobuf.encode_json(json)
+    proto_observation = Google::Protobuf.decode_json(Mjxproto::Observation, json_string)
+end
+
 
 RSpec.describe "action_in_view" do
     it "start_game" do
@@ -18,4 +24,15 @@ RSpec.describe "action_in_view" do
     end
 end
 
-Rspec
+RSpec.describe "forbidden_tile" do
+    file = File.open("spec/resources/observations-000.json", "r")
+    lines = file.readlines
+    it "normal" do
+        observation = observation_from_json(lines,0)
+        init_hand = observation.private_info.init_hand
+    end
+    it "riichi" do  # 聴牌にならないはいを返しているか
+    end
+    it "hora" do # くい変えになる牌を返しているか
+    end
+end
