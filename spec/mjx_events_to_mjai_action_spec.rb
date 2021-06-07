23078952a01491fce1f738d9e908e0260040dc92
Nishimori Soichiro <55940613+nissymori@users.noreply.github.com>
2021-06-08 08:55:08 +0900

    mjx_event→mjai_action テスト修正 (#80)
    * fix test
    
    * add test

--- spec/mjx_events_to_mjai_action_spec.rb
+++ spec/mjx_events_to_mjai_action_spec.rb
@@ -39,74 +39,74 @@ RSpec.describe "mjx_eventの変換" do
         observation = observation_from_json(lines, 3)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
         mjx_event = public_observation_difference[4]
-        expected_mjai_action = {"type"=>"dahai", "actor"=>2, "pai"=>"P", "tsumogiri"=>true}
+        expected_mjai_action = {"type"=>"dahai", "actor"=>2, "pai"=>"W", "tsumogiri"=>true}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event, nil, nil)).to eq expected_mjai_action 
     end
     it "CHI" do
-        previous_public_observation = observation_from_json(lines, 4).public_observation.events
-        observation = observation_from_json(lines, 5)
+        previous_public_observation = observation_from_json(lines, 8).public_observation.events
+        observation = observation_from_json(lines, 9)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
-        mjx_event = public_observation_difference[3]
-        expected_mjai_action = {"type"=>"chi", "actor"=>2, "target"=>1, "pai"=>"4p", "consumed"=>["5p", "6p"]}
+        mjx_event = public_observation_difference[5]
+        expected_mjai_action = {"type"=>"chi", "actor"=>3, "target"=>2, "pai"=>"9p", "consumed"=>["7p", "8p"]}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event, nil, nil)).to eq expected_mjai_action
         #38, 39
     end
     it "PON" do
-        previous_public_observation = observation_from_json(lines, 1).public_observation.events
-        observation = observation_from_json(lines, 2)
+        previous_public_observation = observation_from_json(lines, 6).public_observation.events
+        observation = observation_from_json(lines, 7)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
-        mjx_event = public_observation_difference[1]
-        expected_mjai_action = {"type"=>"pon", "actor"=>1, "target"=>0, "pai"=>"E", "consumed"=>["E", "E"]}
+        mjx_event = public_observation_difference[0]
+        expected_mjai_action = {"type"=>"pon", "actor"=>0, "target"=>1, "pai"=>"4p", "consumed"=>["4p", "4p"]}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event, nil, nil)).to eq expected_mjai_action
         #84, 85
     end
     it "ADDED_KAN" do
-        previous_public_observation = observation_from_json(lines, 8).public_observation.events
-        observation = observation_from_json(lines, 9)
+        previous_public_observation = observation_from_json(lines, 43).public_observation.events
+        observation = observation_from_json(lines, 44)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
-        mjx_event = public_observation_difference[4]
-        expected_mjai_action = {"type"=>"kakan","actor"=>2,"pai"=>"9m","consumed"=>["9m", "9m", "9m"]}
+        mjx_event = public_observation_difference[6]
+        expected_mjai_action = {"type"=>"kakan","actor"=>3,"pai"=>"9p","consumed"=>["9p", "9p", "9p"]}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event, nil, nil)).to eq expected_mjai_action
     end
     it "OPEN_KAN" do
-        previous_public_observation = observation_from_json(lines_2, 129).public_observation.events
-        observation = observation_from_json(lines_2, 130)
+        previous_public_observation = observation_from_json(lines_2, 4).public_observation.events
+        observation = observation_from_json(lines_2, 5)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
-        mjx_event = public_observation_difference[5]
-        expected_mjai_action = {"type"=>"daiminkan", "actor"=>1, "target"=>0, "pai"=>"W", "consumed"=>["W", "W", "W"]}
+        mjx_event = public_observation_difference[7]
+        expected_mjai_action = {"type"=>"daiminkan", "actor"=>0, "target"=>1, "pai"=>"7s", "consumed"=>["7s", "7s", "7s"]}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event, nil, nil)).to eq expected_mjai_action
     end
     it "CLOSED_KAN" do
-        previous_public_observation = observation_from_json(lines, 84).public_observation.events
-        observation = observation_from_json(lines, 85)
+        previous_public_observation = observation_from_json(lines, 297).public_observation.events
+        observation = observation_from_json(lines, 298)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
-        mjx_event = public_observation_difference[0]
-        expected_mjai_action = {"type"=>"ankan","actor"=>0,"consumed"=>["5s", "5s", "5s", "5sr"]}
+        mjx_event = public_observation_difference[4]
+        expected_mjai_action = {"type"=>"ankan","actor"=>2,"consumed"=>["9p", "9p", "9p", "9p"]}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event, nil, nil)).to eq expected_mjai_action
         # 153 154  
     end  
     it "RIICHI" do
-        previous_public_observation = observation_from_json(lines, 40).public_observation.events
-        observation = observation_from_json(lines, 41)
+        previous_public_observation = observation_from_json(lines, 96).public_observation.events
+        observation = observation_from_json(lines, 97)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
-        mjx_event = public_observation_difference[4]
+        mjx_event = public_observation_difference[6]
         expected_mjai_action = {"type"=>"reach","actor"=>3}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event, nil, nil)).to eq expected_mjai_action
     end
     it "RIICHI_SCORE_CHANGE" do
-        previous_public_observation = observation_from_json(lines, 40).public_observation.events
-        observation = observation_from_json(lines, 41)
+        previous_public_observation = observation_from_json(lines, 96).public_observation.events
+        observation = observation_from_json(lines, 97)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
-        mjx_event = public_observation_difference[6]
+        mjx_event = public_observation_difference[8]
         expected_mjai_action = {"type"=>"reach_accepted","actor"=>3,"deltas"=>[0,0,0,-1000],"scores"=>[25000,28900,25000,20100]}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event,nil ,[25000,28900,25000,21100])).to eq expected_mjai_action
     end
     it "NEW_DORA" do
-        previous_public_observation = observation_from_json(lines, 8).public_observation.events
-        observation = observation_from_json(lines, 9)
+        previous_public_observation = observation_from_json(lines, 43).public_observation.events
+        observation = observation_from_json(lines, 44)
         public_observation_difference = trans_server.extract_difference(previous_public_observation, observation)
-        mjx_event = public_observation_difference[6]
-        expected_mjai_action = {"type"=>"dora","dora_marker"=>"4m"}
+        mjx_event = public_observation_difference[8]
+        expected_mjai_action = {"type"=>"dora","dora_marker"=>"4s"}
         expect(mjx_to_mjai.mjx_event_to_mjai_action(mjx_event, nil, nil)).to eq expected_mjai_action
     end                                               
 end
