fd28d0223e12bf4f131c637b25d19ef36d4f1caf
nissymori <kyoudai.630@gmail.com>
2021-10-29 22:48:11 +0900

    pass test

--- spec/mjx_mjai_translater_spec.rb
+++ spec/mjx_mjai_translater_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe TransServer do  # take_actionで実装されている階層の関
 end
 
 
-RSpec.describe "TransServer.take_action()" do  # take_actionで実装されている階層の関数をtest
+RSpec.describe "TransServer Start kyoku" do  # take_actionで実装されている階層の関数をtest
   file = File.open("spec/resources/observations-000.json", "r")
   lines = file.readlines
   trans_server = TransServer.new({:target_id=>0, "test"=>"yes"})
@@ -77,3 +77,69 @@ RSpec.describe "TransServer.take_action()" do  # take_actionで実装されて�
     expect(mjx_actions[-1]).to eq expected_mjx_action
   end
 end
+
+RSpec.describe "TransServer Middle kyoku" do  # take_actionで実装されている階層の関数をtest
+  file = File.open("spec/resources/observations-000.json", "r")
+  lines = file.readlines
+  trans_server = TransServer.new({:target_id=>0, "test"=>"yes"})
+  expected_mjai_actions_middle_1 = [MjaiAction.new({:type=>:dahai, :actor=>0, :pai=>Mjai::Pai.new("9p"), :tsumogiri=>false}), MjaiAction.new({:type=>:tsumo, :actor=>1, :pai=>Mjai::Pai.new("?")}), MjaiAction.new({:type=>:dahai, :actor=>1, :pai=>Mjai::Pai.new("9p"), :tsumogiri=>false}),
+    MjaiAction.new({:type=>:tsumo, :actor=>2, :pai=>Mjai::Pai.new("?")}), MjaiAction.new({:type=>:dahai, :actor=>2, :pai=>Mjai::Pai.new("9m"), :tsumogiri=>false}),
+    MjaiAction.new({:type=>:tsumo, :actor=>3, :pai=>Mjai::Pai.new("?")}), MjaiAction.new({:type=>:dahai, :actor=>3, :pai=>Mjai::Pai.new("3m"), :tsumogiri=>false})]
+  mjai_actions_middle_1 = [MjaiAction.new({:type=>:none}), MjaiAction.new({:type=>:none}), MjaiAction.new({:type=>:none}), MjaiAction.new({:type=>:none}), MjaiAction.new({:type=>:none}),
+    MjaiAction.new({:type=>:none}), MjaiAction.new({:type=>:none}), MjaiAction.new({:type=>:none})]
+  expected_mjai_actions_middle_2 = [MjaiAction.new({:type=>:tsumo, :actor=>0, :pai=>Mjai::Pai.new("6s")})]
+  mjai_actions_middle_2 = [MjaiAction.new({:type=>:dahai, :actor=>0, :pai=>Mjai::Pai.new("8p"), :tsumogiri=>false})]
+  it 'test_take_action_middle' do  # 局の最初  連続で2順
+    previous_events = observation_from_json(lines, 8).public_observation.events
+    observation_1 = observation_from_json(lines, 9)
+    trans_server.set_mjx_events(previous_events)
+    trans_server.observe(observation_1)  # observation→mjai action
+    expected_mjai_actions = expected_mjai_actions_middle_1
+    expect(trans_server.get_mjai_actions()).to eq expected_mjai_actions
+    mjai_actions = mjai_actions_middle_1  # do_actionを通してmjaiのagentから送られてきたmjaiのactionの想定
+    mjx_actions = trans_server.update_next_actions(mjai_actions, observation_1)  # mjai_action→mjx_action
+    expected_mjx_action = observation_1.legal_actions[-1]
+    expect(mjx_actions[-1]).to eq expected_mjx_action
+
+    observation_2 = observation_from_json(lines, 10)
+    trans_server.observe(observation_2)
+    expected_mjai_actions = expected_mjai_actions_middle_2
+    expect(trans_server.get_mjai_actions()).to eq expected_mjai_actions
+    mjai_actions = mjai_actions_middle_2  # do_actionを通してmjaiのagentから送られてきたmjaiのactionの想定
+    mjx_actions = trans_server.update_next_actions(mjai_actions, observation_2)  # mjai_action→mjx_action
+    expected_mjx_action = observation_2.legal_actions[3]
+    expect(mjx_actions[-1]).to eq expected_mjx_action
+  end
+end
+
+RSpec.describe "TransServer end kyoku" do  # take_actionで実装されている階層の関数をtest
+  file = File.open("spec/resources/observations-000.json", "r")
+  lines = file.readlines
+  trans_server = TransServer.new({:target_id=>0, "test"=>"yes"})
+  expected_mjai_actions_end_kyoku_1 = [MjaiAction.new({:type=>:dahai, :actor=>0, :pai=>Mjai::Pai.new("1s"), :tsumogiri=>true}), MjaiAction.new({:type=>:ryukyoku, :reason=>:fonpai, :tehais=>[[Mjai::Pai.new("3m"), Mjai::Pai.new("4m"), Mjai::Pai.new("5m"), Mjai::Pai.new("2p"), Mjai::Pai.new("2p"), Mjai::Pai.new("2p"), Mjai::Pai.new("6s"), Mjai::Pai.new("7s"), Mjai::Pai.new("9s"), Mjai::Pai.new("9s")], [Mjai::Pai.new("5mr"), Mjai::Pai.new("5m"), Mjai::Pai.new("6s"), Mjai::Pai.new("8s")], [Mjai::Pai.new("?")]*13, [Mjai::Pai.new("5p"), Mjai::Pai.new("6p"), Mjai::Pai.new("7p"), Mjai::Pai.new("7p"), Mjai::Pai.new("8p"), Mjai::Pai.new("3s"), Mjai::Pai.new("3s"), Mjai::Pai.new("5sr"), Mjai::Pai.new("5s"), Mjai::Pai.new("5s")]], :tenpais=>[true, true, false, true], :deltas=>[1000, 1000, -3000, 1000], :scores=>[26000, 26000, 22000, 26000]}) ,MjaiAction.new({:type=>:end_kyoku})]
+  mjai_actions_end_kyoku_1 = [MjaiAction.new({:type=>:none}), MjaiAction.new({:type=>:none}), MjaiAction.new({:type=>:none})]
+  expected_mjai_actions_end_kyoku_2 = [MjaiAction.new({:type=>:start_kyoku, :kyoku=>1, :bakaze=>Mjai::Pai.new("E"), :honba=>1, :kyotaku=>0, :oya=>0, :dora_marker=>Mjai::Pai.new("2s"), :tehais=>[[Mjai::Pai.new("9p"), Mjai::Pai.new("5s"), Mjai::Pai.new("N"), Mjai::Pai.new("F"), Mjai::Pai.new("N"), Mjai::Pai.new("2m"), Mjai::Pai.new("9s"), Mjai::Pai.new("7m"), Mjai::Pai.new("4p"), Mjai::Pai.new("N"), Mjai::Pai.new("4s"), Mjai::Pai.new("E"), Mjai::Pai.new("3m")], [Mjai::Pai.new("?")]*13, [Mjai::Pai.new("?")]*13, [Mjai::Pai.new("?")]*13]}) ,MjaiAction.new({:type=>:tsumo, :actor=>0, :pai=>Mjai::Pai.new("5m")})]
+  mjai_actions_end_kyoku_2 = [MjaiAction.new({:type=>:dahai, :actor=>0, :pai=>Mjai::Pai.new("E"), :tsumogiri=>false})]
+  it 'test_take_action_middle' do  # 局の最初  連続で2順
+    previous_events = observation_from_json(lines, 28).public_observation.events
+    observation_1 = observation_from_json(lines, 29)
+    trans_server.set_mjx_events(previous_events)
+    trans_server.observe(observation_1)  # observation→mjai action
+    expected_mjai_actions = expected_mjai_actions_end_kyoku_1
+    expect(trans_server.get_mjai_actions()).to eq expected_mjai_actions
+    mjai_actions = mjai_actions_end_kyoku_1  # do_actionを通してmjaiのagentから送られてきたmjaiのactionの想定
+    mjx_actions = trans_server.update_next_actions(mjai_actions, observation_1)  # mjai_action→mjx_action
+    expected_mjx_action = observation_1.legal_actions[-1]
+    expect(mjx_actions[-1]).to eq expected_mjx_action
+
+    observation_2 = observation_from_json(lines, 30)
+    trans_server.observe(observation_2)
+    expected_mjai_actions = expected_mjai_actions_end_kyoku_2
+    expect(trans_server.get_mjai_actions()).to eq expected_mjai_actions
+    mjai_actions = mjai_actions_end_kyoku_2  # do_actionを通してmjaiのagentから送られてきたmjaiのactionの想定
+    mjx_actions = trans_server.update_next_actions(mjai_actions, observation_2)  # mjai_action→mjx_action
+    expected_mjx_action = observation_2.legal_actions[-3]
+    expect(mjx_actions[-1]).to eq expected_mjx_action
+  end
+end
+
