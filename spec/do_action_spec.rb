09e7b54964a995776d00de37de02add0fb54500f
nissymori <kyoudai.630@gmail.com>
2021-10-30 14:05:08 +0900

    create frame

---
+++ spec/do_action_spec.rb
@@ -0,0 +1,7 @@
+require './lib/mjx_mjai_translater/trans_server'
+require './lib/mjx_mjai_translater/mjx_to_mjai'
+require './lib/mjx_mjai_translater/trans_player'
+require './lib/mjx_mjai_translater/mjai_action_to_mjx_action'
+
+RSpec.describe 'do_action' do
+end
